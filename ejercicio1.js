f52dc4c05fd583bb95f29b45bddb46129938c6b5
Miguel <stormyth2305@gmail.com>
2026-06-07 19:14:23 +0200

    primer ejercicio terminado

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+// 1.1
+let miSuperheroeFavorito = "Hulk";
+console.log(miSuperheroeFavorito);
+
+// 1.2
+let numeroFavorito = 50;
+console.log(numeroFavorito);
+
+// 1.3
+let altura = 5;
+let longitud = 10;
+
+console.log(altura);
+console.log(longitud);
+
+// 1.4
+let suma = altura + longitud;
+console.log(suma);
+
+
+
+
