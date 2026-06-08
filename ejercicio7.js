9a44f275ac4fbb83a00c85f2cfa24ef8baa9545c
Miguel <stormyth2305@gmail.com>
2026-06-08 20:51:59 +0200

    Septimo ejercicio completado

---
+++ ejercicio7.js
@@ -0,0 +1,10 @@
+
+function greaterNumber(numberOne , numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne)
+  } if (numberTwo > numberOne) {
+    console.log(numberTwo)
+  }
+}
+
+greaterNumber(15, 25);
