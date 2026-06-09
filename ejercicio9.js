e4b3e62aa42127a69582d8defe7e8064e9a0c410
Miguel <stormyth2305@gmail.com>
2026-06-09 20:18:05 +0200

    Ejercicio 9 completado

---
+++ ejercicio9.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumNumbers(numberList) {
+  let suma = 0;
+for (const number of numberList) {
+    suma += number;
+}
+console.log(suma);
+return suma;
+
+}
+
+sumNumbers(numbers)
